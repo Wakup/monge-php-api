9d42b4bb39f8819017b45a7b330f0d31a03df550
Alex Gutiérrez <alejandro.gutierrez@twincoders.com>
2019-02-18 11:46:34 +0100

    Included basic documentation for getPaginatedAttributes method

--- src/Client.php
+++ src/Client.php
@@ -13,13 +13,21 @@ use GuzzleHttp\Exception\GuzzleException;
 
 class Client
 {
-
+    /**
+     * Obtains the paginated list of product attributes registered on Wakup catalogue manager
+     *
+     * @param int $page Page to request. First page is 0
+     * @param int $perPage Number of results to obtain per request. Default is 25.
+     * @return PaginatedAttributes Product attributes list with pagination information
+     * @throws GuzzleException Exce
+     * @throws \JsonMapper_Exception
+     */
     public function getPaginatedAttributes($page = 0, $perPage = 25) : PaginatedAttributes
     {
         $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', 'http://ecommerce.wakup.net:9000/catalog/attributes');
-        $body = $response->getBody();
-        $obj = json_decode($body);
+        $response = $client->request('GET', 'http://ecommerce.wakup.net:9000/catalog/attributes',
+            ['query' => ['page' => $page, 'perPage' => $perPage]]);
+        $obj = json_decode($response->getBody());
         $mapper = new \JsonMapper();
         $response = new PaginatedAttributes();
         $mapper->map($obj, $response);
